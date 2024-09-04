b465fc3d4bb66b1d0c57afba6f3698711b2f411a
wx_orange <wx_orange@orange007.com>
2024-09-04 09:59:53 +0800

    更新Card组件增加Shadow DOM

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,15 +1,25 @@
 import PropTypes from 'prop-types'
-import React, { memo } from 'react'
+import React, { memo, useRef, useEffect } from 'react'
 import styled from 'styled-components'
-import parse from 'html-react-parser'
 
 const Card = memo((props) => {
   const { data } = props
+  const containerRef = useRef()
+  useEffect(() => {
+    const sheet = new CSSStyleSheet()
+    sheet.replaceSync(data.css)
+    const container = containerRef.current
+    // 防止反复插入样式
+    if (!container.shadowRoot) {
+      const shadow = container.attachShadow({ mode: 'open' })
+      shadow.adoptedStyleSheets = [sheet]
+      shadow.innerHTML = data.html
+    }
+  }, [data])
 
   return (
     <CardWrapper className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4'>
-      <style title={data.id}>{data.css}</style>
-      {parse(data.html)}
+      <div ref={containerRef}></div>
     </CardWrapper>
   )
 })
@@ -27,4 +37,5 @@ const CardWrapper = styled.div`
   align-items: center;
   justify-content: center;
   background-color: #212121;
+  overflow: hidden;
 `
